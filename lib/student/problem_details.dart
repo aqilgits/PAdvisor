c2e46dda5aa25e0f81f8e0665518d0c64674af67
aqilgits <aqil.dev@gmail.com>
2022-02-01 09:34:43 +0800

    report form done and firebase config

---
+++ lib/student/problem_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProblemDetails extends StatefulWidget {
+  const ProblemDetails({Key? key}) : super(key: key);
+
+  @override
+  _ProblemDetailsState createState() => _ProblemDetailsState();
+}
+
+class _ProblemDetailsState extends State<ProblemDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/student/reportForm.dart
+++ lib/student/reportForm.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart' show FirebaseFirestore;
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ReportForm extends StatefulWidget {
   const ReportForm({Key? key}) : super(key: key);
@@ -11,6 +14,45 @@ class ReportForm extends StatefulWidget {
 class _ReportFormState extends State<ReportForm> {
   String desc = "";
   String title = "";
+  File? file;
+  var storage = FirebaseStorage.instance;
+
+  Future selectFile() async {
+    final result = await FilePicker.platform.pickFiles(allowMultiple: false);
+    if (result == null) return;
+    final path = result.files.single.path!;
+    setState(() {
+      file = File(path);
+    });
+    print("file selected");
+  }
+
+  fileindicator() {
+    if (file == null) {
+      return Text("No file selected");
+    } else {
+      return Text("File selecetd");
+    }
+  }
+
+  Future uploadFileToFirebase() async {
+    var filename = DateTime.now().toString();
+    TaskSnapshot snapshot =
+        await storage.ref().child("File/$filename").putFile(file!);
+
+    if (snapshot.state == TaskState.success) {
+      final String downloadUrl = await snapshot.ref.getDownloadURL();
+      await FirebaseFirestore.instance.collection("Report").add({
+        "url": downloadUrl,
+        "Title": title,
+        "Desc": desc,
+      });
+    } else {
+      print('Error from image repo ${snapshot.state.toString()}');
+      throw ('error');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -98,13 +140,45 @@ class _ReportFormState extends State<ReportForm> {
                   style: TextStyle(fontSize: 20),
                 ),
               ),
+              const SizedBox(height: 10),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red[900],
+                      ),
+                      onPressed: () {
+                        selectFile();
+                      },
+                      child: const Icon(Icons.upload_file),
+                    ),
+                    Container(
+                      child: fileindicator(),
+                    )
+                  ],
+                ),
+              ),
               const SizedBox(height: 20),
               ElevatedButton(
                   onPressed: () async {
-                    await FirebaseFirestore.instance.collection("Report").add({
-                      "Title": title,
-                      "Desc": desc,
-                    });
+                    uploadFileToFirebase();
                     Navigator.pop(context);
                   },
                   child: const Text("Submit"))
