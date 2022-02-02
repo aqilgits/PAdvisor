2b67751681904462bdc6f46b0eacd7888ed34f55
harrislukman13 <harrislukman13@gmail.com>
2022-02-02 18:12:34 +0800

    hod ui

---
+++ lib/hod/hodWrapper.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:fluid_bottom_nav_bar/fluid_bottom_nav_bar.dart';
+import 'package:padvisor/hod/listAnnouncement.dart';
+import 'package:padvisor/hod/listReport.dart';
+
+class HodWrapper extends StatefulWidget {
+  const HodWrapper({Key? key}) : super(key: key);
+
+  @override
+  _HodWrapperState createState() => _HodWrapperState();
+}
+
+class _HodWrapperState extends State<HodWrapper> {
+  Widget? _child;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red[900],
+        title: const Text(
+          "PADVISOR",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.account_circle_outlined),
+            color: Colors.white,
+            iconSize: 40,
+          )
+        ],
+      ),
+      body: _child,
+      bottomNavigationBar: FluidNavBar(
+        icons: [
+          FluidNavBarIcon(
+              icon: Icons.home,
+              backgroundColor: Colors.grey[200],
+              extras: {"label": "home"}),
+          FluidNavBarIcon(
+              icon: Icons.wysiwyg_outlined,
+              backgroundColor: Colors.grey[200],
+              extras: {"label": "report"}),
+        ],
+        onChange: _handleNavigationChange,
+        style: FluidNavBarStyle(
+            iconUnselectedForegroundColor: Colors.red[900],
+            barBackgroundColor: Colors.red[900]),
+        animationFactor: 0.5,
+        scaleFactor: 1.5,
+        defaultIndex: 1,
+        itemBuilder: (icon, item) => Semantics(
+          label: icon.extras!["label"],
+          child: item,
+        ),
+      ),
+    );
+  }
+
+  void _handleNavigationChange(int index) {
+    setState(() {
+      switch (index) {
+        case 0:
+          _child = const ListAnnouncement();
+          break;
+        case 1:
+          _child = const ListReport();
+          break;
+      }
+    });
+  }
+}
---
+++ lib/hod/listAnnouncement.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class ListAnnouncement extends StatefulWidget {
+  const ListAnnouncement({Key? key}) : super(key: key);
+
+  @override
+  _ListAnnouncementState createState() => _ListAnnouncementState();
+}
+
+class _ListAnnouncementState extends State<ListAnnouncement> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+            width: MediaQuery.of(context).size.width,
+            alignment: Alignment.topCenter,
+            child: Container(
+              padding: const EdgeInsets.all(10.0),
+              alignment: Alignment.centerLeft,
+              height: 50,
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Announcement",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: const CircleBorder(),
+                        primary: Colors.red[900],
+                        shadowColor: Colors.red[900]),
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
